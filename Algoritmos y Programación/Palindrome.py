1e3c179193ad6c92f186f60ee4889a20daa618e7
victorpointud <113309762+victorpointud@users.noreply.github.com>
2024-07-08 18:18:06 -0400

    Todo

--- Algoritmos y Programación/Palindrome.py
+++
@@ -1,8 +0,0 @@
-numero_palabra = input("Ingrese un número o palabra:")
-
-numero_palabra[:: -1]
-
-if numero_palabra == numero_palabra[:: -1]:
-    print("Es palíndromo")
-else:
-    print("No es palíndromo")
